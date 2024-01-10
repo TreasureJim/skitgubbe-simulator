1740ae60506f979451f70e1c875ba0d54e9a2d00
Liam Boreback <boreback.work@gmail.com>
2024-01-11 00:48:50 +0100

    Begin work on queue system

--- server/server.rs
+++ server/server.rs
@@ -1,21 +1,84 @@
 mod deck;
 mod game;
-mod game_server;
 
-use std::{net::TcpListener, io::{BufWriter, Write}};
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        State, WebSocketUpgrade,
+    },
+    http::Method,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+};
+use tokio::{self, net::TcpListener};
+use tower_http::cors::{Any, CorsLayer};
 
-fn main() {
-    let address = std::env::args().skip(1).next().unwrap_or("127.0.0.1:0".to_string());
+struct ServerQueue {
+    queue: Mutex<VecDeque<User>>,
+}
 
-    let server = TcpListener::bind(address).expect("Couldn't bind to address");
+use uuid;
+
+pub struct User {
+    id: uuid::Uuid,
+    socket: WebSocket
+}
+
+impl User {
+    pub fn new(socket: WebSocket) -> Self {
+        Self { id: uuid::Uuid::new_v4(), socket }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let port;
+    {
+        let port_env = std::env::var("PORT");
+        if port_env.is_ok() {
+            port = port_env
+                .unwrap()
+                .parse::<u16>()
+                .expect("PORT env var is not a number");
+        } else {
+            // default
+            eprintln!("No PORT env var set defaulting to port 0");
+            port = 0;
+        }
+    }
+
+    let address = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+    let server = TcpListener::bind(address)
+        .await
+        .expect("Couldn't bind to address");
     println!("Listening on: {}", server.local_addr().unwrap());
 
-    let (user_stream, _addr) = server.accept().unwrap();
+    let queue_state = Arc::new(ServerQueue {
+        queue: Mutex::new(VecDeque::new()),
+    });
 
-    let mut writer = BufWriter::new(user_stream.try_clone().unwrap());
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(vec![Method::GET]);
 
-    writeln!(&mut writer, "hello :D").unwrap();
-    writer.flush().unwrap();
+    let router = Router::new()
+        .route("/ws", get(handler))
+        .with_state(queue_state)
+        .layer(cors);
 
-    user_stream.shutdown(std::net::Shutdown::Both).unwrap();
+    axum::serve(server, router).await.unwrap();
+}
+
+async fn handler(ws: WebSocketUpgrade, State(state): State<Arc<ServerQueue>>) -> impl IntoResponse {
+    ws.on_upgrade(|socket| handle_socket(socket, state))
+}
+
+async fn handle_socket(socket: WebSocket, state: Arc<ServerQueue>) {
+    let user = User::new(socket);
+    state.queue.lock().unwrap().push_back(user);
 }
