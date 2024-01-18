ed65951241faa2b713025726c6c269d43fcfb438
Liam Boreback <boreback.work@gmail.com>
2024-01-18 10:46:13 +0100

    Work on connecting with client

--- client/client.rs
+++ client/client.rs
@@ -1,17 +1,29 @@
-use std::{net::TcpStream, io::{BufReader, BufRead}};
+use tokio_tungstenite;
+use url;
+use futures_util::{SinkExt, StreamExt};
+use log::*;
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{Error, Result},
+};
+use futures_util::StreamExt;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let addr = std::env::args().nth(1).expect("parsing cli address arg");
-    let stream = TcpStream::connect(addr).expect("connecting to address");
+    let url = url::Url::parse(&(format!("ws://{addr}/queue"))).expect("Invalid ws url");
 
-    let mut reader = BufReader::new(stream.try_clone().unwrap());
+    let (stream, _) = tokio_tungstenite::connect_async(&url)
+        .await
+        .expect("connecting to address");
+    println!("Connected to {addr}");
 
-    loop {
-        let mut s = String::new();
-        let bytes_read = reader.read_line(&mut s).unwrap();
-        if bytes_read == 0 {
-            return;
+    let (mut read , mut write) = stream.split();
+
+    while let Some(message) = read.next().await {
+        match message {
+            Ok(msg) => println!("Received a message: {}", msg),
+            Err(e) => eprintln!("Error receiving message: {}", e),
         }
-        print!("{}", s);
     }
 }
